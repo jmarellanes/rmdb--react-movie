eba7e3e0d7e69d5f2b73fac228a5f4d66b665acd
jmarellanes <josemanuel.arellanes@gmail.com>
2021-07-18 12:24:53 -0600

    Fetch data from the API for Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,40 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
+import API from "../API";
+
+// Hook
 
 const Home = () => {
 	const [state, setState] = useState();
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(false);
 
+	const fetchMovies = async (page, searchTerm = "") => {
+		try {
+			setError(false);
+			setLoading(true);
 
-	return <div>Home Page</div>
-}
+			const movies = await API.fetchMovies(searchTerm, page);
+
+			setState((prev) => ({
+				...movies,
+				results:
+					page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+			}));
+		} catch (error) {
+			setError(true);
+		}
+
+		setLoading(false);
+	};
+
+	// Initial render
+	useEffect(() => {
+		fetchMovies(1);
+	}, []); // Dependency array: Trigger use effect when Home component mount at initial render
+
+	console.log(state);
+
+	return <div>Home Page</div>;
+};
 
 export default Home;
