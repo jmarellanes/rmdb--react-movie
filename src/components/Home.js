08fe01d3ed27898912d74602425b2b4717c6fa8f
jmarellanes <josemanuel.arellanes@gmail.com>
2021-07-18 12:41:32 -0600

    Custom hook for Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,9 @@
 import { useState, useEffect } from "react";
 import API from "../API";
-
-// Hook
+import { useHomeFetch } from "../hooks/useHomeFetch";
 
 const Home = () => {
-	const [state, setState] = useState();
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(false);
-
-	const fetchMovies = async (page, searchTerm = "") => {
-		try {
-			setError(false);
-			setLoading(true);
-
-			const movies = await API.fetchMovies(searchTerm, page);
-
-			setState((prev) => ({
-				...movies,
-				results:
-					page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
-			}));
-		} catch (error) {
-			setError(true);
-		}
-
-		setLoading(false);
-	};
-
-	// Initial render
-	useEffect(() => {
-		fetchMovies(1);
-	}, []); // Dependency array: Trigger use effect when Home component mount at initial render
-
+	const { state, loading, error } = useHomeFetch();
 	console.log(state);
 
 	return <div>Home Page</div>;
