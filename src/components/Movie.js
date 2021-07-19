1f4cdb4985e578178af40a48bc642de8ea9c278c
jmarellanes <josemanuel.arellanes@gmail.com>
2021-07-19 15:29:47 -0600

    Add - BreadCrumb component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 // Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 // Components
+import BreadCrumb from './BreadCrumb';
 import Grid from './Grid';
 import Spinner from './Spinner';
 // Hook
@@ -11,13 +12,15 @@ import NoImage from '../images/no_image.jpg';
 
 const Movie = () => {
 	const { movieIdPath } = useParams();
-
 	const { state: movie, loading, error } = useMovieFetch(movieIdPath);
+
+	if (loading) return <Spinner />;
+	if (error) return <div>Something went wrong...</div>;
 
 	console.log(movie);
 	return (
 		<>
-			<div>Movie</div>
+			<BreadCrumb movieTitle={movie.original_title} />
 		</>
 	);
 };
